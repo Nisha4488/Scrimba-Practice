fc8a42a5130b88622a1f11633f774a6ba1c736b9
Nisha Bharti <me.nishabharti@gmail.com>
2019-07-28 22:40:26 -0400

    calling data from external file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import Joke from './Joke'
+import jokeData from './jokeData'
 
 
 function App(){
+    // const nums = [1,2,3,4,5,6]
+    // const doubleNum = nums.map((function(num){
+    //     return num*2
+    // }))
+
+    // console.log(doubleNum)
+    const jokeComponent = jokeData.map(joke=><Joke key={joke.id} question={joke.question} answer={joke.answer}/>)
     return(
         <div>
-            <Joke punchline="Hello"/>
-            <Joke question="Second" punchline="Hi"/>
-            <Joke  punchline="Hey"/>
-            <Joke  punchline="Hola"/>
-            <Joke question="Fifth" punchline="Good"/>
+            {jokeComponent}
         </div>
     )
 }
---
+++ src/jokeData.js
@@ -0,0 +1,24 @@
+const jokeData =[
+    {id:1,
+    
+    answer:"It's one"
+},
+{id:2,
+    question:"Two",
+    answer:"It's two"
+},
+{id:1,
+    question:"Three",
+    answer:"It's three"
+},
+{id:1,
+    question:"Four",
+    answer:"It's four"
+},
+{id:1,
+    question:"Five",
+    answer:"It's five"
+}
+]
+
+export default jokeData
